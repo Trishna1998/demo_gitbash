79608d5f6d1933b1d7752173e550901ad447dddb
Trishna1998 <89373220+Trishna1998@users.noreply.github.com>
2021-08-23 14:13:33 +0530

    1st program

---
+++ Firstprgm/Firstprgm/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Firstprgm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //##.....printing statement
+
+            //Console.WriteLine("Hello mindteck");
+            //Console.ReadLine();
+
+            //##......add two number
+
+            /*int a,b,sum;
+            Console.WriteLine("Enter 1st number:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter 2nd number:");
+            b = Convert.ToInt32(Console.ReadLine());
+            sum = a + b;
+            Console.WriteLine("Total is:"+ sum);
+            Console.ReadLine();*/
+
+
+            //##....Typecasting-float to int
+
+            float x = 3.78F;
+            int d = (int)x;
+            Console.WriteLine(x);
+            Console.WriteLine(d);
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
